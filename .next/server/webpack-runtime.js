627307ddd6ad4a1f659bc462f91d367f1d936cbb
alperen-tunc <tuncalperen@mail.com>
2024-08-20 21:34:37 +0200

    gitignore all

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("af29e1bb1726ff2e")
+/******/ 		__webpack_require__.h = () => ("ff61af051f60372f")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,9 @@
 export default function NotFound() {
     return(
-        <div><h1>Page not found</h1><p>could not find requested resource</p></div>
+        <div>
+            <h1>Page not found</h1>
+            <p>could not find requested resource</p>
+        </div>
     );
 }
 
